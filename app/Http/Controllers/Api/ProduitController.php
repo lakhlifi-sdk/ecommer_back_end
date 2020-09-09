d74b7e53b234f568b69cfb085333d8978b05a3bb
lakhlifi-sdk <57691109+lakhlifi-sdk@users.noreply.github.com>
2020-09-09 18:08:47 +0100

    create product update 9

--- app/Http/Controllers/Api/ProduitController.php
+++ app/Http/Controllers/Api/ProduitController.php
@@ -50,12 +50,13 @@ class ProduitController extends Controller
         $product->description=$req->description;
         $product->prix=$req->prix;
         $product->promotion=$req->promotion;
-        if($req->photo!=''){
+        $product->image_url="https://upload.wikimedia.org/wikipedia/commons/c/ca/Boston_skyline_from_Longfellow_Bridge_September_2017_panorama_2.jpg"
+        /*if($req->photo!=''){
         $photo=time().'.jpg';
         file_put_contents('storage/public/produits/'.$photo,base64_decode($req->photo));
         $product->image_url=$photo;
         
-        }
+        }*/
         $product->save();
 
         return response()->json([
